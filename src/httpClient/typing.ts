3b0691c6c382ca1b1f4a3ea539bb35c676dbf7f7
ligaopeng123 <gaopeng@123>
2021-08-09 17:47:19 +0800

    合入人：pgli 合入问题: 模板优化 合入时间：2021年8月9日17:47:14

--- src/httpClient/typing.ts
+++ src/httpClient/typing.ts
@@ -9,16 +9,6 @@
  * @版权所有: pgli
  *
  **********************************************************************/
-type OptionBase = {
-	body?: any,
-	[propName: string]: any;
-}
-export type Option = OptionBase & {
-	method: 'GET' | 'POST' | 'PUT' | 'DELETE',
-}
-
-export type Fetch = (url: string, option?: OptionBase) => any;
-
 export type GraphqlOption = {
 	fetchPolicy?: string;
 	[propName: string]: any;
--- src/pages/admin/menus/index.tsx
+++ src/pages/admin/menus/index.tsx
@@ -11,11 +11,24 @@
  */
 
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {withRouter} from 'react-router-dom';
 import BasicContents from '@components/Contents/BasicContents';
+import {get, post} from "@/httpClient";
 
 const RouterAdminMenus: React.FC<{}> = (props: any) => {
+	useEffect(() => {
+		post('/json/menus.json', {body: {a: 1}}).then((res: any) => {
+			console.log(res);
+		}).catch((e: Error) => {
+			console.log(e)
+		});
+		get('/json/menus.json', {params: {a: 1}, responseType: 'blob'}).then((res: any) => {
+			console.log(res);
+		}).catch((e: Error) => {
+			console.log(e)
+		});
+	}, []);
 	return (
 		<BasicContents>
 			菜单表
