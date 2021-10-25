ab662a0b29e6eec34db97e9db862aa134504f06d
ligaopeng123 <gaopeng@123>
2021-10-25 17:32:16 +0800

    基本项目 模板优化 增加表格参数处理

---
+++ src/share/utils.ts
@@ -0,0 +1,31 @@
+/**********************************************************************
+ *
+ * @模块名称: utils
+ *
+ * @模块用途: utils
+ *
+ * @date: 2021/10/25 17:21
+ *
+ * @版权所有: pgli
+ *
+ **********************************************************************/
+import {removeEmptyParams} from '@gaopeng123/utils';
+
+/**
+ * 处理表格参数
+ * @param params
+ * @param pageOptions
+ */
+export const dealWithParams = (params: any, pageOptions?: any) => {
+	const {pageNumKey, pageSizeKey} = Object.assign({pageNumKey: 'pageNum', pageSizeKey: 'pageSize'}, pageOptions);
+	const newParams = Object.assign({}, removeEmptyParams(params));
+	newParams[pageNumKey] = newParams.current;
+	newParams[pageSizeKey] = newParams.pageSize;
+	if (pageNumKey !== 'current') {
+		delete newParams.current;
+	}
+	if (pageSizeKey !== 'pageSize') {
+		delete newParams.pageSize;
+	}
+	return newParams;
+};
