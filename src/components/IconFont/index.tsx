32863938156b4b96ca90710783d747a4df249c18
ligaopeng123 <gaopeng@123>
2021-12-24 11:12:28 +0800

    模板bug修改 能力集成

---
+++ src/components/IconFont/index.tsx
@@ -0,0 +1,9 @@
+import {createFromIconfontCN} from '@ant-design/icons';
+
+export const IconFont = createFromIconfontCN({
+    scriptUrl: [
+        '/js/iconfont.js', // icon-javascript, icon-java, icon-shoppingcart (overrided)
+    ],
+});
+
+export default IconFont;
