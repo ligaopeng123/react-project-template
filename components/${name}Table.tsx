1ad2a32007842cf6f0831418f3666a490196bea5
ligaopeng123 <gaopeng@123>
2022-06-15 13:56:10 +0800

    列定制

--- components/${name}Table.tsx
+++ components/${name}Table.tsx
@@ -194,6 +194,10 @@ const <%= name %>Table: React.FC<TableProps> = (props) => {
 					<AppstoreAddOutlined/>新建
 				</Button>
 			]}
+			columnsState={{
+				persistenceKey: '<%= name %>-columnsState',
+				persistenceType: 'localStorage'
+			}}
 		/>
 	);
 };
