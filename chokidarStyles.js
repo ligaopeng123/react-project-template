cd6014e6f80484b92219378c4f07a3906e8cbabd
ligaopeng123 <1170234932@qq.com>
2024-08-09 23:53:12 +0800

    fix: upperCamelName调整2

--- chokidarStyles.js
+++ chokidarStyles.js
@@ -7,7 +7,7 @@ const postcss = require('postcss');
 
 // 要监听的文件或目录路径
 const filePath = './src/styles.scss';
-const templatePath = './src/<%= upperCamelName %>Template.ts';
+const templatePath = './src/' + <%= upperCamelName %>Template.ts';
 
 // 监听文件或目录变化
 const watcher = chokidar.watch(filePath);
